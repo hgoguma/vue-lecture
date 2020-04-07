5a0d5f86a0816d03683867e46dddc426395b306e
hyeleelee <hgoguma124@gmail.com>
2020-04-07 21:50:19 +0900

    검색 폼 구현

--- Vanilla.js/js/views/FormView.js
+++ Vanilla.js/js/views/FormView.js
@@ -19,10 +19,22 @@ FormView.showResetBtn = function(show = true) {
 
 FormView.bindEvents = function() {
     this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+    this.resetEl.addEventListener('click', e => this.onClickReset(e))
 }
 
-FormView.onKeyup = function() {
+FormView.onKeyup = function(e) {
+    const enter = 13
     this.showResetBtn(this.inputEl.value.length)
+    if(!this.inputEl.value.length) this.emit('@reset')
+    if (e.keyCode !== enter) return 
+    //todo... 검색 결과를 보여줘야 함 -> formView는 mainController에 엔터키가 입력되었음을 알려줌
+    this.emit('@submit', {input: this.inputEl.value})
+    
+}
+
+FormView.onClickReset = function() {
+    this.emit('@reset')
+    this.showResetBtn(false)
 }
 
 export default FormView
