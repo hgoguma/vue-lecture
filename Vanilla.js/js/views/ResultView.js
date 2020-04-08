2f405d7250a1aff45e849f8c677c17019d8e5e9a
hyeleelee <hgoguma124@gmail.com>
2020-04-08 09:22:50 +0900

    검색 결과 구현중

--- Vanilla.js/js/views/ResultView.js
+++ Vanilla.js/js/views/ResultView.js
@@ -4,6 +4,10 @@ const tag = '[ResultView]'
 
 const ResultView = Object.create(View)
 
+ResultView.message = {
+    NO_RESULT: '검색 결과가 없습니다'
+}
+
 ResultView.setup = function(el) {
     this.init(el)
 }
@@ -11,11 +15,21 @@ ResultView.setup = function(el) {
 //검색 결과 보여주기
 ResultView.render = function(data = []) {
     console.log(tag, 'render()', data)
-    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : '검색 결과가 없습니다'
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.message.NO_RESULT
 }
 
 ResultView.getSearchResultsHtml = function(data) {
-    debugger
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item)
+        return html
+    }, '<ul>') + '</ul>'
+}
+
+ResultView.getSearchItemHtml = function(item) {
+    return `<li>
+    <img src="${item.image}">
+    <p>${item.name}</p>
+    </li>`
 }
 
 export default ResultView
