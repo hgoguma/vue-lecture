4d708a8af47de82e3ccd0e1a7d42940d2413a1d5
hyeleelee <hgoguma124@gmail.com>
2020-04-10 15:52:53 +0900

    검색 폼 구현

--- Vue.js/js/app.js
+++ Vue.js/js/app.js
@@ -1,6 +1,21 @@
 new Vue({
     el: '#app', //vue 인스턴스가 어느 부분에 마운팅 될지 설정
     data : {
-        msg: 'hello world'
+        query: '' //입력 데이터 받아서 저장
+    },
+    methods: {
+        onSubmit(e) {
+            debugger
+        },
+        onReset() {
+            this.query = ''
+            //todo 검색 결과를 숨기기
+            debugger
+        },
+        onKeyup() {
+            if(!this.query.length) {
+                this.onReset()
+            }
+        }
     }
 })
