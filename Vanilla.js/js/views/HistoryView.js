c1d9f073d2527081c157481d2f361a733a79e7c6
hyeleelee <hgoguma124@gmail.com>
2020-04-10 15:40:38 +0900

    Vue.js 적용

--- Vanilla.js/js/views/HistoryView.js
+++ Vanilla.js/js/views/HistoryView.js
@@ -31,9 +31,4 @@ HistoryView.onRemove = function(keyword) {
     this.emit('@remove', { keyword })
 }
 
-HistoryView.insertKeyword = function(keyword) {
-    console.log('insertKeyword')
-    console.log(keyword)
-}
-
 export default HistoryView
---
+++ Vue.js/js/app.js
@@ -0,0 +1,6 @@
+new Vue({
+    el: '#app', //vue 인스턴스가 어느 부분에 마운팅 될지 설정
+    data : {
+        msg: 'hello world'
+    }
+})
